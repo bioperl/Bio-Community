7eb937ff973fc3ddeb6a190ef8a5e9db083524ae
Florent Angly <florent.angly@gmail.com>
2011-12-17 14:52:32 +1000

    Index is now simply an array of offsets instead of an array of array of offsets

--- lib/Bio/Community/Role/IndexedTable.pm
+++ lib/Bio/Community/Role/IndexedTable.pm
@@ -9,9 +9,21 @@ use namespace::autoclean;
 requires '_fh',
          '_readline';
 
+=head1 Constructors
+
+=head2 delim
+
+ Title   : delim
+ Usage   : my $delim = $in->delim;
+ Function: Get or set the delimited, i.e. the characters that delimit the
+           columns of the table. The default is tab, "\t".
+ Args    : A string
+ Returns : A string
+
+=cut
 
 has 'delim' => (
-   is => 'rw',
+   is => 'ro',
    isa => 'Str',
    required => 0,
    init_arg => '-delim',
@@ -20,19 +32,46 @@ has 'delim' => (
 );
 
 
-#### provide a start and stop so that the entire file does not need to be
-#### indexed: headers and footers do not need to be indexed
+=head2 start_line
+
+ Title   : start_line
+ Usage   : my $line_num = $in->start_line;
+ Function: Get or set the line number at which the table starts. The default is
+           1, i.e. the table starts at the beginning of the file.
+ Args    : A strictly positive number
+ Returns : A strictly positive number
+
+=cut
+
+#has 'start_line' => (
+#   is => 'ro',
+#   isa => 'StrictlyPositiveInt',
+#   required => 0,
+#   init_arg => '-start_line',
+#   default => 1,
+#   lazy => 1,
+#);
 
 
-has '_index' => (
-   is => 'rw',
-   isa => 'ArrayRef[ArrayRef[PositiveInt]]',
-   required => 0,
-   init_arg => undef,
-   predicate => '_has_index',
-   #default => sub { [] },
-   #lazy => 1,
-);
+=head2 end_line
+
+ Title   : end_line
+ Usage   : my $line_num = $in->end_line;
+ Function: Get or set the line number at which the table ends. If undef (the
+           default), the table ends at the last line of the file.
+ Args    : A strictly positive number or undef
+ Returns : A strictly positive number or undef
+
+=cut
+
+#has 'end_line' => (
+#   is => 'ro',
+#   isa => 'Maybe[StrictlyPositiveInt]',
+#   required => 0,
+#   init_arg => '-end_line',
+#   default => undef,
+#   lazy => 1,
+#);
 
 
 =head2 _max_line
@@ -81,6 +120,19 @@ method BUILD {
 }
 
 
+has '_index' => (
+   is => 'rw',
+   ####
+   #isa => 'ArrayRef[ArrayRef[PositiveInt]]',
+   isa => 'ArrayRef[PositiveInt]',
+   ####
+   required => 0,
+   init_arg => undef,
+   default => sub { [] },
+   lazy => 1,
+);
+
+
 =head2 _index_table
 
  Title   : _index
@@ -94,9 +146,13 @@ method BUILD {
 method _index_table () {
    # Index the file the first time
 
-   my @arr = (); # an array of array 
-
+   my @arr = ( 0 ); # array of file offsets 
    my ($max_line, $max_col) = (0, 0);
+
+   #### TODO: provide a start and end line
+   #my $start_line = $self->start_line;
+   #my $end_line   = $self->end_line;
+   ####
 
    my $file_offset = 0;
    while (my $line = $self->_readline(-raw => 1)) {
@@ -127,7 +183,7 @@ method _index_table () {
          }
       }
       $max_line++;
-      push @arr, \@matches;
+      push @arr, @matches;
    }
    $self->_index(\@arr);
    $self->_max_line($max_line);
@@ -158,29 +214,22 @@ method _index_table () {
 
 method _get_indexed_value (StrictlyPositiveInt $line, StrictlyPositiveInt $column) {
    my $val = undef;
+   if ( ($line <= $self->_max_line) && ($column <= $self->_max_col) ) {
 
-   my $offset1;
-   if ($column == 1) {
-      if ($line == 1) {
-         $offset1 = 0;
-      } else {
-         $offset1 = $self->_index->[$line-2]->[-1]
-      }
-   } else {
-      $offset1 = $self->_index->[$line-1]->[$column-2];
-   }
-   
-   if (defined $offset1) {
-      seek $self->_fh, $offset1, 0;
-      my $offset2 = $self->_index->[$line-1]->[$column-1];
-      if (defined $offset2) {
-         read($self->_fh, $val, $offset2 - $offset1) or
-            $self->throw("Error: Could not read content between offset $offset1 and $offset2\n$!\n");
-         my $delim = $self->delim;
-         $val =~ s/[\r\n$delim]//g;
-      }
-   }
+      # Retrieve the value if it is within the bounds of the table
+      my $pos = ($line - 1) * $self->_max_col + $column - 1;
+      my $index = $self->_index;
+      my $offset = $index->[$pos];
+      seek $self->_fh, $offset, 0;
+      my $length = $index->[$pos+1] - $offset;
+      read $self->_fh, $val, $length or
+         $self->throw("Error: Could not read $length chars from offset $offset\n$!\n");
 
+      # Clean up delimiters and end of line characters
+      my $delim = $self->delim;
+      $val =~ s/[\r\n$delim]//g;
+
+   }
    return $val;
 }
 
