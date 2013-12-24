cecadfb38aeeaa49fd4a5f8451263782da578bca
Florent Angly <florent.angly@gmail.com>
2013-12-24 17:14:33 +0100

    Email address fix

--- lib/Bio/Community/Tools/Transformer.pm
+++ lib/Bio/Community/Tools/Transformer.pm
@@ -48,7 +48,7 @@ L<https://redmine.open-bio.org/projects/bioperl/>
 
 =head1 COPYRIGHT
 
-Copyright 2011,2012,2013 by Florent Angly <florent.angly.com>
+Copyright 2011,2012,2013 by Florent Angly <florent.angly@gmail.com>
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.10.1 or,
